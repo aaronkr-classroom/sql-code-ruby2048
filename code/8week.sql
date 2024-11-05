6dfa15e3f87c7aa4eee0e83ae9e9eeb1b4ebe740
ruby2048 <133571195+ruby2048@users.noreply.github.com>
2024-11-05 11:24:12 +0900

    Add files via upload

---
+++ code/8week.sql
@@ -0,0 +1,14 @@
+SELECT * FROM 학생;
+
+-- 트랜잭션
+BEGIN; --START TRANSATION;
+DELETE FROM 학생 WHERE 학생.성별 = '남';
+DELETE FROM 학생 WHERE 학생.성별 = '여';
+SELECT * FROM 학생;
+ROLLBACK;
+SELECT * FROM 학생;
+
+BEGIN;
+UPDATE 학생 SET 이름 = '홍길순' WHERE 학번 = 's002';
+SELECT * FROM 학생;
+COMMIT;
